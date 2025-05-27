d192ce1339f04b03fb65db73bc0c38f37c14d17d
iacostap23 <iacostap@eafit.edu.co>
2025-05-27 10:48:42 -0500

    BD

--- Grattia_Project/settings.py
+++ Grattia_Project/settings.py
@@ -1,20 +1,36 @@
 from pathlib import Path
 import os 
+from dotenv import load_dotenv
 
-
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+DEBUG = os.getenv('DEBUG') == 'True'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dz=k1l2dgzq(dm)-z4%hjb!$3c3bgj@e!zo1aln_sef)qusxb4'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
@@ -67,16 +83,7 @@ WSGI_APPLICATION = 'Grattia_Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'grattia',
-        'USER': 'root',
-        'PASSWORD': 'Papi1970*',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
+
 
 # settings.py
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'  # Usando la base de datos para almacenar sesiones
