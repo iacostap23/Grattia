455ddb95f545e2f5e007cf830c06d67d2b026153
Isabella Acosta Pareja <143555705+iacostap23@users.noreply.github.com>
2025-05-27 12:39:33 -0500

    Update settings.py

--- Grattia_Project/settings.py
+++ Grattia_Project/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-+9jg)*p09$at+zck$n-6*djf=34w4sc*m##^fmph4pb$_nh%xb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['52.207.219.114']
+ALLOWED_HOSTS = ['3.91.200.27']
 
 
 # Application definition
